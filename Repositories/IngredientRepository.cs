a4b464f97ccbeebb2b72a85063872dd12948f608
Chase16Rogers <chase16rogers@gmail.com>
2021-02-22 08:10:00 -0700

    init

---
+++ Repositories/IngredientRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using all_spice.Models;
+
+namespace all_spice.Repositories
+{
+  public class IngredientRepository
+  {
+    internal IEnumerable<Ingredient> GetAll()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Ingredient Create(Ingredient newIng)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
